d419df1fbc1a09ce08fcf75ce29c20807c1b3b43
xSAMRATx <mdabdulazizsamrat@gmail.com>
2025-07-31 07:13:03 +0600

    Some Changes

--- resources/js/components/StatCard.vue
+++ resources/js/components/StatCard.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="flex border p-4 rounded shadow-md">
     <div class="flex flex-col flex-1 space-y-2">
-      <!-- Icon Circle -->
       <div class="p-2 bg-indigo-100 text-indigo-600 rounded w-10 h-10 flex items-center justify-center mb-4">
         <component :is="icon" class="w-5 h-5 text-black" />
       </div>
 
-      <!-- Info -->
       <div class="text-md text-gray-500">{{ title }}</div>
       <div class="text-3xl font-semibold text-gray-900">{{ value }}</div>
 
     </div>
-    <!-- Figma Styled Badge -->
+
     <div class="flex flex-col justify-end ml-4">
       <span :class="[
         'text-xs font-medium px-3 py-1 rounded-full',
@@ -19,8 +17,6 @@
           ? 'bg-[#ECFDF3] text-[#039855]'
           : 'bg-[#FEF3F2] text-[#D92D20]'
       ]">{{ trend > 0 ? `▲ ${trend}%` : `▼ ${Math.abs(trend)}%` }}</span>
-
-
     </div>
   </div>
 </template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,7 +2,6 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 import StatCard from '@/components/StatCard.vue';
 import MonthlyTarget from '@/components/MonthlyTarget.vue';
 import { Users, Box, Target, BarChart } from 'lucide-vue-next'
@@ -10,7 +9,6 @@ import MonthlySalesChart from '@/components/MonthlySalesChart.vue';
 import Statistics from '@/components/Statistics.vue';
 import CustomersDemographic from '@/components/CustomersDemographic.vue';
 import RecentOrders from '@/components/RecentOrders.vue';
-import Navbar from '@/components/Navbar.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -23,33 +21,31 @@ const breadcrumbs: BreadcrumbItem[] = [
 <template>
 
     <Head title="Dashboard" />
-    
+
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                <StatCard title="Customers" value="3,782" :trend="11.01" :icon="Users" class="col-span-1 row-span-1" />
-                <StatCard title="Orders" value="5,359" :trend="-9.05" :icon="Box" class="col-span-1 row-span-1" />
+                <StatCard title="Customers" value="3,782" :trend="11.01" :icon="Users" class="w-full" />
+                <StatCard title="Orders" value="5,359" :trend="-9.05" :icon="Box" class="w-full" />
 
-                <MonthlyTarget class="col-span-2 row-span-2" />
+                <MonthlyTarget class="w-full sm:col-span-2 lg:col-span-2 lg:row-span-2 order-2" />
 
-                <div class="col-span-2 row-start-2 bg-white rounded-xl p-6 shadow">
-                    <MonthlySalesChart class="col-span-2 row-span-2" />
+                <div
+                    class="w-full sm:col-span-2 lg:col-span-2 lg:row-start-2 bg-white rounded-xl p-6 shadow order-1 sm:order-none overflow-x-auto">
+                    <div class="min-w-[300px] w-full">
+                        <MonthlySalesChart class="w-full"/>
+                    </div>
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 xl:grid-cols-1 gap-6 mb-6">
+            <div class="grid grid-cols-1 gap-6 mb-6">
                 <Statistics />
             </div>
 
-            <!-- Bottom Section -->
             <div class="grid grid-cols-1 xl:grid-cols-2 gap-6">
                 <CustomersDemographic />
                 <RecentOrders />
-            </div>
-            <div
-                class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
             </div>
         </div>
     </AppLayout>
